72ac07655f238d874285d4189bdd471371a410df
173gvQY8MFaDk9Jt <g@v.q>
2026-04-26 22:31:35 +0800

    init

---
+++ client/index.ts
@@ -0,0 +1,10 @@
+import { Context } from "@koishijs/client";
+
+import "./index.scss";
+
+export default function (ctx: Context) {
+  ctx.theme({
+    id: "pink-light",
+    name: "Pink Light",
+  });
+}
